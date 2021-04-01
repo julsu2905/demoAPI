30b4e213a1639c0850e97957f90202cebbe50333
sujul2905 <51090620+kurokami2905@users.noreply.github.com>
2021-04-02 01:19:31 +0700

    update failure authen route

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,23 @@
 var express = require("express");
 var apiRouter = express.Router();
 const passport = require("passport");
-const userController = require('../controllers/user.controller')
+const userController = require("../controllers/user.controller");
 
 /* GET home page. */
-apiRouter.get("/login", );
+apiRouter.get("/login");
 
 apiRouter.get(
-	"/auth/google",userController.createUser,
+	"/auth/google",
+	userController.createUser,
 	passport.authenticate("google", {
 		scope: ["profile", "email"],
 	})
 );
-apiRouter.get('/auth/google/callback',passport.authenticate('google'))
+apiRouter.get(
+	"/auth/google/callback",
+	passport.authenticate("google", { failureRedirect: "/login" }),
+	(req, res) => {
+		res.redirect("/login");
+	}
+);
 module.exports = apiRouter;
